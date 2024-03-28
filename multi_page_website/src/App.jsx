c064a3455b6c7bad95ae8b223335f9eab479cc64
CoolShivas <shivas2710cool00@gmail.com>
2024-03-28 23:22:13 +0530

    BrowserRouter_Routes_Route_React_Router_Dom

--- multi_page_website/src/App.jsx
+++ multi_page_website/src/App.jsx
@@ -1,3 +1,5 @@
+import ErrorPage from "./components/ErrorPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./components/AboutPage";
 import ServicePage from "./components/ServicePage";
 import ContactPage from "./components/ContactPage";
@@ -7,13 +9,16 @@ import AppName from "./components/AppName";
 
 
 function App() {
-  return <>
-    <AppName></AppName>
-    <HomePage></HomePage>
-    <ContactPage></ContactPage>
-    <ServicePage></ServicePage>
-    <AboutPage></AboutPage>
-  </>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" Component={HomePage}></Route>
+      <Route path="/contact" Component={ContactPage}></Route>
+      <Route path="/about" Component={AboutPage}></Route>
+      <Route path="/appname" Component={AppName}></Route>
+      <Route path="/servicepage/:id" Component={ServicePage}></Route>
+      <Route path="*" Component={ErrorPage}></Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
