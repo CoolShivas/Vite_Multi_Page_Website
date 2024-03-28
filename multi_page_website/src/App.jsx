cc9331502135e53047321435bc9782f4aa170de3
CoolShivas <shivas2710cool00@gmail.com>
2024-03-28 22:59:37 +0530

    Initialization_of_multipage_website

---
+++ multi_page_website/src/App.jsx
@@ -0,0 +1,19 @@
+import AboutPage from "./components/AboutPage";
+import ServicePage from "./components/ServicePage";
+import ContactPage from "./components/ContactPage";
+import HomePage from "./components/HomePage";
+import AppName from "./components/AppName";
+
+
+
+function App() {
+  return <>
+    <AppName></AppName>
+    <HomePage></HomePage>
+    <ContactPage></ContactPage>
+    <ServicePage></ServicePage>
+    <AboutPage></AboutPage>
+  </>
+}
+
+export default App;
---
+++ multi_page_website/src/components/AppName.jsx
@@ -0,0 +1,9 @@
+
+
+const AppName = () => {
+    return (
+        <h1> Multipage Website </h1>
+    )
+}
+
+export default AppName
